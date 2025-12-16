d1d616709f1c32db7b19f95295886f607acb40d3
SegaFlomer <SegaFlomer@gmail.com>
2025-12-16 19:21:36 -0300

    Agregando gitignore y arreglos de diseño

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ruta para la página de inicio
 @app.route('/')
-def inicio():
-    # Esto es lo que hace que se abra tu index.html
+def home():
     return render_template('index.html')
+
+# Ruta para la página de productos (Aquí es donde mandas al usuario)
+@app.route('/productos')
+def pagina_productos():
+    return render_template('productos.html') # Necesitas crear este archivo html
+
+# Ruta para contacto
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html') # Necesitas crear este archivo html
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
